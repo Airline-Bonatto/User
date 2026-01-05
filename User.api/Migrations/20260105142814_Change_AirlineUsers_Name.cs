f51b2505d41b23fef44403f2a78b235feed84e06
Gabriel Bonatto Justo <gjusto@nelogica.com.br>
2026-01-05 11:30:09 -0300

    Update AirlineUsers columns names.

---
+++ User.api/Migrations/20260105142814_Change_AirlineUsers_Name.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace User.api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Change_AirlineUsers_Name : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Password",
+                table: "AirlineUsers",
+                newName: "password");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "AirlineUsers",
+                newName: "name");
+
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "AirlineUsers",
+                newName: "lastName");
+
+            migrationBuilder.RenameColumn(
+                name: "Email",
+                table: "AirlineUsers",
+                newName: "email");
+
+            migrationBuilder.RenameColumn(
+                name: "Document",
+                table: "AirlineUsers",
+                newName: "document");
+
+            migrationBuilder.RenameColumn(
+                name: "AirlineUserId",
+                table: "AirlineUsers",
+                newName: "airlineUserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "password",
+                table: "AirlineUsers",
+                newName: "Password");
+
+            migrationBuilder.RenameColumn(
+                name: "name",
+                table: "AirlineUsers",
+                newName: "Name");
+
+            migrationBuilder.RenameColumn(
+                name: "lastName",
+                table: "AirlineUsers",
+                newName: "LastName");
+
+            migrationBuilder.RenameColumn(
+                name: "email",
+                table: "AirlineUsers",
+                newName: "Email");
+
+            migrationBuilder.RenameColumn(
+                name: "document",
+                table: "AirlineUsers",
+                newName: "Document");
+
+            migrationBuilder.RenameColumn(
+                name: "airlineUserId",
+                table: "AirlineUsers",
+                newName: "AirlineUserId");
+        }
+    }
+}
--- User.api/Models/AirlineUser.cs
+++ User.api/Models/AirlineUser.cs
@@ -1,14 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 using User.api.Requests;
 
 namespace User.api.Models;
 
+[Table("AirlineUsers")]
 public class AirlineUser
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Column("airlineUserId")]
     public int AirlineUserId { get; set; }
+
+    [Column("email")]
     public string Email { get; set; } = string.Empty;
+
+    [Column("password")]
     public string Password { get; set; } = string.Empty;
+
+    [Column("document")]
     public string Document { get; set; } = string.Empty;
+
+    [Column("name")]
     public string Name { get; set; } = string.Empty;
+
+    [Column("lastName")]
     public string LastName { get; set; } = string.Empty;
 
 
